59cdd5a506d22fb9e0f815295531e3236ae5a1fc
xiaoxingaha <chengxing.zhuang@foxmail.com>
2014-06-19 15:45:12 +0800

    修改了 lcdTest.c文件中的 void __irq RxInt()串口通信中断处理函数 搞懂了各部分的功能，注释了Uart_Getch()、Uart_SendByte(r);

--- lcd_2410_test/LCDDriver/lcdTest.c
+++ lcd_2410_test/LCDDriver/lcdTest.c
@@ -6,7 +6,7 @@
 #include "PIC1.h"
 #include "PIC2.h"
 int const Time_1s= 1000;
-int const Time_half_s= 100;
+int const Time_half_s= 10000;
 void Lcd2410_Init_On(void);
 extern U16 (*frameBuffer16BitTft800480)[SCR_XSIZE_TFT_800480];
 
@@ -25,7 +25,7 @@ void var_Init()
 {
 	 flag=1;//切换图片标志
      Time1ms = 0 ;//定时器计时
-	 pre_key='1';
+	 pre_key='1';//上一个输入字符
 	 Time_count=Time_1s;//定时器计时上限1000
 	 change_flag=0;
 }
@@ -111,32 +111,45 @@ void __irq RxInt()
     rSUBSRCPND=BIT_SUB_RXD0;//0x4a000018 清除RXD1,ERR1中断挂起标志位
  	rSRCPND=BIT_UART0;  //0x4a000000
     rINTPND=BIT_UART0;	//)0x4a000010
+	
+	//Uart_Getch()和Uart_GetKey()的区别：
+	//前者Uart_Getch()是等待用户在超级终端输入数据后才继续往下运行，否则一直循环等待，直到有数据输入
+	//而后者Uart_GetKey()则是若有用户输入数据则返回输入的数据，若没有则返回0，不等待
 	r = Uart_GetKey();
 	if(r!='1'&&r!='0')
 	{
+		//将输入的r，发送到发送缓存寄存器，然后显示在超级终端
 		Uart_SendByte(r);
+		Uart_Printf("\n输入模式无效。\n");
 		//change_flag=0;
 		return;
 	}
+	//相同的模式就不进行切换
 	else if(r!=pre_key)
 	{
 		if(pre_key=='1')
 		{
 			Time_count=Time_half_s;
+			Uart_SendByte(r);
+			Uart_Printf("\n进入10s切换一张图片模式。\n");
 			pre_key=r;
 		}
 		else if(pre_key=='0')
 		{
 			Time_count=Time_1s;
+			Uart_SendByte(r);
+			Uart_Printf("\n进入1s切换一张图片模式。\n");
 			pre_key=r;
 		}
 		
 		
 	}
 	else
+	{
 		// change_flag=0;
-	
-	Uart_SendByte(r);
+		Uart_SendByte(r);
+		Uart_Printf("\n切换图片模式不变。\n");
+	}
 }
 
 
